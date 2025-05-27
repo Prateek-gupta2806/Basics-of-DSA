3c8e86fa6adfed9e13676ce96fc21bc2e78ec684
Prateek-gupta2806 <coolrishabh122@gmail.com>
2025-05-27 22:45:29 +0530

    pattern4

--- Pattern3,4.js
+++ Pattern3,4.js
@@ -33,3 +33,45 @@ function pattern3 (n) {for (let row = 1; row<= n; row +=1) {
   ****
  *****
 ******      */
+
+/* Q4 Print the pattern  pyrAMID */
+
+function pattern4 (n) {for (let row = 1; row<= n; row +=1) {
+   let str = "";
+   let spaces = n-row;
+   /* we will concatinate the spaces*/
+   for (let j=1; j<= spaces ; j+= 1){
+       str += " ";
+   }
+   /*after spaces concatination we will concatinate stars*/
+   for ( let k=1; k<= 2*row-1; k+= 1){
+       str += "*";
+   }
+   console.log(str);
+  };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
