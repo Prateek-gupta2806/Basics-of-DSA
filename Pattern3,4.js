a522a4b1af5a9648c6cbc71c34b71d71cc0e639a
Prateek-gupta2806 <coolrishabh122@gmail.com>
2025-05-27 21:28:41 +0530

    push

---
+++ Pattern3,4.js
@@ -0,0 +1,25 @@
+console.log("Making new file");
+
+/*Q3  for a value "n" make pattern 
+ if n = 3 
+
+      *
+     **
+    ***      */
+
+/*solution*/
+
+function pattern (n) {for (let row = 1; row<= n; row +=1) {
+   let str = "";
+   let spaces = n-row;
+   for (let j=1; j<= spaces ; j+= 1){
+       str += " ";
+   }
+   for ( let k=1; k<= row; k+= 1){
+       str += "*";
+   }
+   console.log(str);
+  };
+};
+
+pattern (6);
