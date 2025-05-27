7e7224243f643baabedaf0fb39bb69ee266640e6
Prateek-gupta2806 <coolrishabh122@gmail.com>
2025-05-27 19:38:59 +0530

    initial

---
+++ first.js
@@ -0,0 +1,22 @@
+/*pattern 1*/
+
+function pattern(n) {
+   for (let row = 1; row <= n; row +=1) {
+  let str = "";
+  for (let col = 1; col <= n; col +=1) {
+     str += "*"
+  };
+  console.log(str);
+};
+
+}
+
+pattern(5);
+
+pattern (3);
+
+pattern (4);
+
+
+
+
