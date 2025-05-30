66e40f0cd9313fb20c679aa6f0c49c482ff7a50a
Prateek-gupta2806 <coolrishabh122@gmail.com>
2025-05-30 20:28:58 +0530

    message

--- pattern,next.js
+++ pattern,next.js
@@ -51,7 +51,19 @@ function pattern1 (n) {for (let row = 1; row<= n; row +=1) {
 
 /*Q8 new PATTERN */
 
- function upper (n) {for (let row = 1; row<= n-(n-3); row +=1) {
+
+/*SOlution :-
+
+Row no = (n-1)/2
+
+Left star = right star = row number. 
+spaces logic = N- 2*row. 
+
+
+
+*/
+
+ function upper (n) {for (let row = 1; row <= (n-1)/2; row +=1) {
     let str = "";
     for (let i =1; i<=row; i+=1) {
         str+= "*";
@@ -80,16 +92,18 @@ function pattern1 (n) {for (let row = 1; row<= n; row +=1) {
 };
 
  
- function lower (n) {for (let row = 1; row<= n-(n-3); row +=1) {
+ function lower (n) {for (let row = 1; row<= (n-1)/2; row +=1) {
     let str = "";
-    for (let i =1; i<=row; i+=1) {
+    let spaces = 2*row-1;
+    let star = (n-spaces)/2;
+    for (let i =1; i<=star; i+=1) {
         str+= "*";
     }
-    let spaces = 2*row-1;
+    
     for (let j=1; j<= spaces ; j+= 1){
         str += " ";
     }
-    for ( let k=1; k<= row; k+= 1){
+    for ( let k=1; k<= star; k+= 1){
         str += "*";
     }
     console.log(str);
@@ -98,6 +112,20 @@ function pattern1 (n) {for (let row = 1; row<= n; row +=1) {
  
 
 
+ function patternMentos1 (n) {
+    upper(n);
+    mid(n);
+    lower(n);
+};
+
+
+
+/* Tasks  
+1. Patterns complete 
+2. Namaste JS next video..
+3. Madam video age.. 
+4. Notes dekhne hai ki ab kaise revise krne hai.....
+***/
 
 
 
