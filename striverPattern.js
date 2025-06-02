fdc45ad7fc8884d6353c3591959b6c669a67ed3d
Prateek-gupta2806 <coolrishabh122@gmail.com>
2025-06-02 22:27:32 +0530

    push

---
+++ striverPattern.js
@@ -0,0 +1,21 @@
+
+/*Pattern12*/
+
+
+function pattern12 (n) {
+    
+let num = 0;
+    
+    for (let row = 1; row <= n; row +=1) {
+    let str = "";
+    for (let i =1; i<=row; i=i+1) {
+        num = num + 1
+        str += num + ' ' ;
+        
+    }
+    console.log(str);
+    
+   };
+ };
+
+ 
